d5ec6d1cb57b4eb30ee013b4e0e25aa3a7534ea0
Dariusz Luczynski <LuczynskiDar@users.noreply.github.com>
2026-03-05 18:32:05 +0000

    feat(17-custom-actions); finishing python script

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,3 +1,36 @@
-if __name__ == '__main__':
+import os
+import requests
+import time
 
-    print("Hello world")
+def ping_url(website_url, delay, max_trials):
+    trials = 0
+    while trials < max_trials:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"Website {url} is reachable")
+                return True
+        except requests.ConnectionError:
+            print(f"Website {url} is unreachable. Retrying in {delay} seconds ...")
+            time.sleep(delay)
+            trials += 1
+        except requests.exceptions.MissingSchema:
+            print(f"Invalid URL format: {url}. Make sure the URL has a valid schema (e.g., Http:// or https://)")
+            return False
+    return False
+
+def run():
+    website_url = os.getenv("INPUT_URL")
+    # website_url = os.environ["INPUT_URL"]
+    delay = os.getenv("INPUT_DELAY")
+    max_trials = os.getenv("INPUT_MAX_TRIALS")
+
+    website_reachable = ping_url(website_url, delay, max_trials)
+
+    if not website_reachable:
+        raise Exception(f"Website {website_url} is malformed or unreachable.")
+    
+    print(f"Website {website_url} is reachable")
+
+if __name__ == '__main__':
+    run()
